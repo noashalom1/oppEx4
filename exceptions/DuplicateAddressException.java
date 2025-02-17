225bfefdef61639b2a1d000044bf009a9e33085f
noashalom1 <noapatito123@gmail.com>
2025-02-17 17:10:36 +0200

    first

--- exceptions/DuplicateAddressException.java
+++ exceptions/DuplicateAddressException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-// Exception when trying to add a property with the same address
+// Exception when trying to add a property with the same address.
 public class DuplicateAddressException extends Exception {
     public DuplicateAddressException(String message) {
         super(message);
